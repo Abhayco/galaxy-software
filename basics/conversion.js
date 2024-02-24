dcafae31af47692c966580457b752832900df945
Abhayco <160998086+Abhayco@users.noreply.github.com>
2024-02-24 07:27:05 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+let a="abc123"
+let vl = Number(a)
+let bool = Number(b)
+console.log(typeof vl);
+console.log(typeof bool);
+
+/*
+"33" => 33
+"33ab" => NaN   (Not a Number)
+true => 1
+false => 0
+*/
+let b=true
+let numbool = Number(a)
+console.log(typeof numbool);
+// 
--- basics/tut3_datatypes.js
+++ basics/tut3_datatypes.js
@@ -3,4 +3,4 @@ console.log(typeof age);//string
 var age1=123;
 console.log(typeof age1);//number
 console.log(typeof null);//object
-console.log(typeof undefined);//object
+console.log(typeof undefined);//undefined
