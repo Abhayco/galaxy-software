1f3545b83808da658fa3dc9674a327f6401356da
Abhayco <160998086+Abhayco@users.noreply.github.com>
2024-02-24 05:19:32 +0000

    datatypes

--- basics/tut2.js
+++ basics/tut2.js
@@ -1,5 +1,7 @@
 let a="abhay";
 var name ="Jay";
+
+
 const state="gujarat";
 
 console.table([a,name,state]);
---
+++ basics/tut3_datatypes.js
@@ -0,0 +1,6 @@
+var age="age";
+console.log(typeof age);//string
+var age1=123;
+console.log(typeof age1);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//object
